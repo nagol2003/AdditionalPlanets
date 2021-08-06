bdd23f659ef4669f1ce837f29abec2592eabfc83
ROM <rom.void95@gmail.com>
2021-08-06 03:15:27 -0500

    Added OreDict method to show example useage

--- src/main/java/io/github/nagol2003/blocks/Polulos/PolulosFerminiumOre.java
+++ src/main/java/io/github/nagol2003/blocks/Polulos/PolulosFerminiumOre.java
@@ -1,41 +1,42 @@
 package io.github.nagol2003.blocks.Polulos;
 
-import java.util.Random;
-
+import io.github.nagol2003.blocks.interfaces.IAddRecipe;
 import io.github.nagol2003.init.AdditionalPlanetsTab;
-import io.github.nagol2003.init.InitBlocks;
-import io.github.nagol2003.init.InitItems;
 import micdoodle8.mods.galacticraft.api.block.ITerraformableBlock;
 import micdoodle8.mods.galacticraft.core.blocks.ISortableBlock;
 import micdoodle8.mods.galacticraft.core.util.EnumSortCategoryBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.oredict.OreDictionary;
 
-public class PolulosFerminiumOre extends Block implements ISortableBlock, ITerraformableBlock {
+public class PolulosFerminiumOre extends Block implements ISortableBlock, ITerraformableBlock, IAddRecipe {
 
+	public PolulosFerminiumOre() {
+		super(Material.ROCK);
+		this.setSoundType(SoundType.STONE);
+		this.setHardness(5.0f);
+		this.setHarvestLevel("pickaxe", 2);
+		this.setCreativeTab(AdditionalPlanetsTab.blockTab);
 
-	        public PolulosFerminiumOre() {
-	    		super(Material.ROCK);
-	    		this.setSoundType(SoundType.STONE);
-	    		this.setHardness(5.0f);
-	    		this.setHarvestLevel("pickaxe", 2);
-	    		this.setCreativeTab(AdditionalPlanetsTab.blockTab);
+	}
 
-	    	}
+	@Override
+	public boolean isTerraformable(World world, BlockPos pos) {
+		return false;
+	}
 
-	    	@Override
-	    	public boolean isTerraformable(World world, BlockPos pos) {
-	    		return false;
-	    	}
+	@Override
+	public EnumSortCategoryBlock getCategory(int meta) {
+		return EnumSortCategoryBlock.DECORATION;
+	}
 
-	    	@Override
-	    	public EnumSortCategoryBlock getCategory(int meta) {
-	    		return EnumSortCategoryBlock.DECORATION;
-	    	}
-	        
+	@Override
+	public void addOreDict() {
+	    OreDictionary.registerOre("oreFerminium", new ItemStack(this));
+	}
+
 }
