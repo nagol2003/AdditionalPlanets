260b400f95ffac1c1cb4840aa5f652892130bbfe
ROM <rom.void95@gmail.com>
2021-08-13 20:07:23 -0500

    Using Fluid bucket as ingredient in recipe

--- src/main/java/io/github/nagol2003/items/ItemFerminiumChunk.java
+++ src/main/java/io/github/nagol2003/items/ItemFerminiumChunk.java
@@ -1,5 +1,9 @@
 package io.github.nagol2003.items;
 
+import io.github.nagol2003.blocks.interfaces.IAddRecipe;
+import io.github.nagol2003.init.InitFluids;
+import io.github.nagol2003.registry.recipe.RecipeBuilder;
+import io.github.nagol2003.util.FluidUtility;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.MobEffects;
@@ -8,15 +12,32 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
-public class ItemFerminiumChunk extends Item {
+public class ItemFerminiumChunk extends Item implements IAddRecipe {
 
 	@Override
-	public void onUpdate(final ItemStack stack, final World worldIn, final Entity entityIn, final int itemSlot, final boolean isSelected) { 
+	public void onUpdate(final ItemStack stack, final World worldIn, final Entity entityIn, final int itemSlot, final boolean isSelected) {
 	    super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
 
-	    if (entityIn instanceof EntityLivingBase) { 
+	    if (entityIn instanceof EntityLivingBase) {
 	        ((EntityLivingBase) entityIn).addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 10, 1));
 	        }
 	}
-	
+
+	@Override
+	public void addRecipes(RecipeBuilder recipes) {
+		//Examples of how to add
+		// either define the itemstacks as method variables
+
+		// we want to produce this item so call this
+		ItemStack output = new ItemStack(this);
+
+		// we want to have 1 tin bucket produce this item, use our own FluidUtility class to pass our fluid and get a bucket as an ItemStack
+		ItemStack input = FluidUtility.getBucket(InitFluids.TIN);
+		//now add
+		recipes.addShapeless("test_chunk", output, input);
+
+
+		// or we can just define the recipe with output and inputs in the method
+		//recipes.addShapeless("test_chunk", new ItemStack(this), FluidUtility.getBucket(InitFluids.TIN));
+	}
 }
